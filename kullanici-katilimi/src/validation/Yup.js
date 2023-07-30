fb6023e83dbfc23342d175d21ea7681095ea8497
burcukocal~ <burcukocal97@gmail.comgit config --global user.name burcukocal~git config --global user.email burcukocal97@gmail.com>
2023-07-30 15:27:27 +0300

    formSchema oluşturuldu

---
+++ kullanici-katilimi/src/validation/Yup.js
@@ -0,0 +1,24 @@
+import * as Yup from "yup";
+
+export const formSchema = Yup.object().shape({
+    firstName: Yup
+        .string()
+        .trim()
+        .required("Lütfen isiminizi giriniz"),
+    surname: Yup
+        .string()
+        .trim()
+        .required("Lütfen soyisiminizi giriniz"),
+    email: Yup
+        .string()
+        .email("Lütfen mail adresinizi giriniz")
+        .required(),
+    password: Yup
+        .string()
+        .required("Lütfen şifrenizi giriniz")
+        .min(5, "Şifreniz en az 5 karakterden oluşmalıdır"),
+    terms: Yup
+        .boolean()
+        .oneOf([true], "Kullanım şartlarını onaylamalısınız")
+        .required()
+});
